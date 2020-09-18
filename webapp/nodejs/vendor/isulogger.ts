1ed2c042ed8aa3cb9aacf528e0aa32f7d4c527eb
Komine Shunta <kmnshnt@gmail.com>
2020-09-18 15:37:27 +0900

    Send bulk

--- webapp/nodejs/vendor/isulogger.ts
+++ webapp/nodejs/vendor/isulogger.ts
@@ -1,25 +1,38 @@
 import urljoin from 'url-join';
 import fetch from 'node-fetch';
 
+interface Data {
+    tag: string;
+    time: string;
+    data: object;
+}
+
+let queue: Data[] = [];
+
 export class IsuLogger {
-    constructor(public endpoint: string, public appID: string) {}
+    constructor(public endpoint: string, public appID: string) {
+        setInterval(() => {
+            if (!queue.length) return;
+            this.request(queue);
+            queue = [];
+        }, 3000);
+    }
 
     async send(tag: string, data: object) {
-        this.request('/send', {
+        this.enqueue({
             tag,
             time: new Date().toISOString().replace(/\.[0-9]{3}Z/, '+09:00'),
             data,
         });
     }
 
-    private async request(path: string, data: object) {
-        const url = urljoin(this.endpoint, path);
+    private async request(data: Data[]) {
+        const url = urljoin(this.endpoint, '/send_bulk');
         const body = JSON.stringify(data);
         const headers = {
             'Content-Type': 'application/json',
             Authorization: 'Bearer ' + this.appID,
         };
-
         const res = await fetch(url, { body, headers, method: 'POST' });
         if (res.status >= 300) {
             throw new Error(
@@ -28,5 +41,9 @@ export class IsuLogger {
                 } ${await res.text()}`
             );
         }
+    }
+
+    private async enqueue(data: Data) {
+        queue.push(data);
     }
 }
